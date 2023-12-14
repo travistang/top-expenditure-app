cc15030668ab34ada775d362706bcbca9b45f67e
Travis Tang <travis.tang@tum.de>
2023-12-14 09:21:16 +0100

    :construction: TEA-28: WIP: Regular income edit form

--- src/components/RegularExpenditurePage/RegularIncomeDetailPage/RegularIncomeForm/index.tsx
+++ src/components/RegularExpenditurePage/RegularIncomeDetailPage/RegularIncomeForm/index.tsx
@@ -1,11 +1,13 @@
-import { IncomeWithId } from "../../../../domain/income";
+import { RegularIncomeWithId } from "../../../../domain/income";
 import { Updater } from "../../../../utils/objects";
 import AmountDisplayInput from "../../../AmountInput/AmountDisplayInput";
+import DateInput from "../../../DateInput";
 import TextDisplayInput from "../../../TextInput/TextDisplayInput";
+import RepeatIntervalDisplayInput from "../../CreateRegularExpenditureModal/RepeatIntervalForm/RepeatIntervalDisplayInput";
 
 type Props = {
-  income: IncomeWithId;
-  updater: Updater<IncomeWithId>;
+  income: RegularIncomeWithId;
+  updater: Updater<RegularIncomeWithId>;
 };
 export default function RegularIncomeForm({ income, updater }: Props) {
   return (
@@ -19,6 +21,25 @@ export default function RegularIncomeForm({ income, updater }: Props) {
         amount={income.amount}
         onChange={updater("amount")}
         className="col-span-full"
+      />
+      <DateInput
+        nullable
+        date={income.startDate}
+        onChange={updater("startDate")}
+        className="col-span-6"
+        label="Start at"
+      />
+      <DateInput
+        nullable
+        date={income.endDate}
+        onChange={updater("endDate")}
+        className="col-span-6"
+        label="Ends at"
+      />
+      <RepeatIntervalDisplayInput
+        className="col-span-full"
+        settings={income.repeat}
+        onChange={updater("repeat")}
       />
     </div>
   );
